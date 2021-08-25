8f177258096048393cff70dd1bc4dac767047538
Fajoce <89431787+Fajoce@users.noreply.github.com>
2021-08-25 11:09:27 -0500

    Update CitySelector.jsx

--- CitySelector.jsx
+++ CitySelector.jsx
@@ -1,45 +1,31 @@
 import React, {useState} from 'react';
 import {Row, Col, FormControl, Button} from 'react-bootstrap';
-import { API_BASE_URL, API_KEY } from '../Config';
 
-const CitySelector = () => {
+
+const CitySelector = ({onSearch}) => {
   const [city, setCity] = useState('');
-  const [results, setResults] = useState(null);
-  const onSearch = () => {
-    fetch(`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
-      .then((response) => response.json())
-      .then((result) => console.log(result));
-  };
-  const onKeyDown = (event) => {
-    if (event.keyCode === 13) {
-      onSearch();
-    }
-  };
+
   return (
     <>
       <Row>
         <Col>
-          <h1>Search your city</h1>
+          <h5>Introduce the name of the city to search</h5>
         </Col>
       </Row>
 
       <Row>
-        {/* xs={4} takes the one third  of the page*/}
-        <Col xs={4} className="text-center container">
-          <FormControl 
+        <Col xs={4}>
+          <FormControl
             placeholder="Enter city"
-            // update city value with the user's input
             onChange={(event) => setCity(event.target.value)}
-            // value will be the currently selected city            
             value={city}
-            onKeyDown={onKeyDown}
           />
         </Col>
       </Row>
-<br></br>
+
       <Row>
         <Col>
-          {/* event handler for button click */}
+         {/* don't forget to edit our function  */}
           <Button onClick={() => onSearch(city)}>Check Weather</Button>
         </Col>
       </Row>
